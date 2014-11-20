09fb43008193cf418b543c5f441f8ccfb2081a12
root <root@app.local>
2014-11-20 19:31:45 +0900

    Jasmine add (javascript test code)

---
+++ JavascriptTest/spec/javascripts/online_statuses.spec.js
@@ -0,0 +1,35 @@
+/**
+ * OnlineStatuses Test
+ *
+ * @return {void}
+ */
+describe('OnlineStatuses Test', function() {
+
+  /*
+   * beforeEach it毎の前処理
+   */
+  //load module
+  beforeEach(module('NetCommonsApp'));
+
+  //controller
+  beforeEach(inject(function($controller) {
+    //spec body
+    scope = {};
+    var OnlineStatusesController = $controller('OnlineStatuses', { $scope: scope });
+    expect(OnlineStatusesController).toBeDefined();
+  }));
+
+  /*
+   * Test
+   */
+  it('OnlineStatuses#initialize()', inject(function($controller) {
+    var frameId = 1;
+    var online_status = 'test value';
+    scope.initialize(frameId, online_status);
+
+    // 同一値チェック
+    expect(scope.frameId).toBe(frameId);
+    expect(scope.visibleManage).toBe(false);
+    expect(scope.online_status).toBe(online_status);
+  }));
+});
---
+++ JavascriptTest/spec/javascripts/online_statuses_edit.spec.js
@@ -0,0 +1,72 @@
+/**
+ * OnlineStatuses.edit Test
+ *
+ * @return {void}
+ */
+describe('OnlineStatuses.edit Test', function() {
+
+  /*
+   * beforeEach it毎の前処理
+   */
+  //load module
+  beforeEach(module('NetCommonsApp'));
+
+  //controller
+  beforeEach(inject(function($controller) {
+    //spec body
+    var frameId = 1;
+    scope = {
+      online_status: {
+        OnlineFrameSetting: {
+          id: 1,
+          display_visitor: 'true',
+          display_login_user: 'true',
+          display_registration_user: 'true'
+        }
+      }
+    };
+    scope.frameId = frameId;
+    var OnlineStatusesEditController = $controller('OnlineStatuses.edit', { $scope: scope });
+    expect(OnlineStatusesEditController).toBeDefined();
+  }));
+
+  /*
+   * Test Case
+   */
+  it('OnlineStatuses.edit#initialize()', inject(function($controller) {
+
+    //$scope.edit.data = {
+    //  OnlineFrameSetting: {
+    //    id:
+    //      $scope.online_status.OnlineFrameSetting.id,
+    //    display_visitor:
+    //      $scope.online_status.OnlineFrameSetting.display_visitor,
+    //    display_login_user:
+    //      $scope.online_status.OnlineFrameSetting.display_login_user,
+    //    display_registration_user:
+    //      $scope.online_status.OnlineFrameSetting.
+    //        display_registration_user
+    //  },
+    //  Frame: {
+    //    id: $scope.frameId
+    //  },
+    //  _Token: {
+    //    key: '',
+    //    fields: '',
+    //    unlocked: ''
+    //  }
+    //};
+
+    scope.initialize();
+    // 同一値チェック
+    expect(scope.edit.data.OnlineFrameSetting.id).toBe(scope.online_status.OnlineFrameSetting.id);
+    expect(scope.edit.data.OnlineFrameSetting.display_visitor).toBe(scope.online_status.OnlineFrameSetting.display_visitor);
+    expect(scope.edit.data.OnlineFrameSetting.display_login_user).toBe(scope.online_status.OnlineFrameSetting.display_login_user);
+    expect(scope.edit.data.OnlineFrameSetting.display_registration_user).toBe(scope.online_status.OnlineFrameSetting.display_registration_user);
+    expect(scope.edit.data.Frame.id).toBe(scope.frameId);
+    expect(scope.edit.data._Token.key).toBe('');
+    expect(scope.edit.data._Token.fields).toBe('');
+    expect(scope.edit.data._Token.unlocked).toBe('');
+  }));
+
+});
