c1a24bc6382a20ec79f889d7dcb72f3285ca9028
root <root@app.local>
2014-11-20 14:16:09 +0900

    phpdoc mod

--- Config/routes.php
+++ Config/routes.php
@@ -2,9 +2,11 @@
 /**
  * OnlineStatuses routes configuration
  *
+ * @author Noriko Arai <arai@nii.ac.jp>
  * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
  * @link http://www.netcommons.org NetCommons Project
  * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
  */
 
 Router::connect('/auth/:action', array(
